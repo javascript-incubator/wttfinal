b9058a500820c7a6b63903fd2b9755ed5723bdef
Rajat Sharma <lunasunkaiser@gmail.com>
2016-03-23 00:45:22 +0530

    Update web.js

--- web.js
+++ web.js
@@ -4,5 +4,8 @@ var morgan = require('morgan');
 var app = express();
 
 app.use(morgan('dev'));
-app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+app.get('/', function(req, res) {
+    res.redirect('http://www.watchthethrones.com');
+});
+//app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 app.listen(process.env.PORT || 5000);
