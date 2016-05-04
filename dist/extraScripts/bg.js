00c9cbf0dfa9ba832f39b43c848f2ce4de0b3bb2
rajatsharma305 <lunasunkaiser@gmail.com>
2016-05-04 19:04:28 +0530

    minorchange3

---
+++ dist/extraScripts/bg.js
@@ -0,0 +1,6 @@
+$("#example, body").vegas({
+    slides: [
+        { src: 'http://i.lv3.hbo.com/assets/images/series/game-of-thrones/episodes/6/52/episode-52-1024.jpg' }
+    ],
+    overlay: true
+});
